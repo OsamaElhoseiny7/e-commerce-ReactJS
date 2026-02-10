6dc9f67033ebc604762485b2badae0318f19f053
OsamaElhosiny7 <osamaelhusseini@gmail.com>
2026-02-10 21:14:56 +0200

    new css

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -8,15 +8,26 @@ const Categories = () => {
     const [show, setShow]  = useState(0)
     const products = useContext(ProductsData)
     const searchResults = useContext(SearchResults)
+    const pageHeight = window.innerHeight
     const categories = ['T-Shirts','Pants','Jackets','Hoodies','Dresses','Shoes','Socks','Accesorries','Suites']
     const [category, setCategory] = useState([])
 
     const initializingProducts = ()=>{
+        if(pageHeight<=430){
+            category?.length>=4 ? setShow(4) : setShow(category?.length)
+        }
+        else{
             category?.length>=5 ? setShow(5) : setShow(category?.length)
+        }
     }
 
     const handleShow = ()=>{
-        show===category?.length ? setShow(5) : show+5 > category?.length ? setShow(category?.length) : setShow(show+5)
+        if(pageHeight<=430){
+            show===category?.length ? setShow(4) : show+4 > category?.length ? setShow(category?.length) : setShow(show+4)
+        }
+        else{
+            show===category?.length ? setShow(5) : show+5 > category?.length ? setShow(category?.length) : setShow(show+5)
+        }
     }
 
     const handelCategory = (event)=>{
@@ -54,7 +65,7 @@ const Categories = () => {
                     )
                 })}
                 </div>
-                  {category?.length>5 &&<div className={styles.showmore}><button onClick={handleShow}>{category?.length===show? 'show less' : 'show more'}</button></div>}
+                  {(category?.length>5 || category?.length>4) &&<div className={styles.showmore}><button onClick={handleShow}>{category?.length===show? 'show less' : 'show more'}</button></div>}
             </div>
         }
     </>
