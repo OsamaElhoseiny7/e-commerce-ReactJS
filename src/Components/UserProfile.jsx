b0215ed5f58f8bd1a555f3e92559881946dbbd74
OsamaElhosiny7 <osamaelhusseini@gmail.com>
2026-02-26 22:57:57 +0200

    new

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -209,20 +209,22 @@ const UserProfile = () => {
         <div className={styles.leftblock} style={{width:cart?.length===0&&favs?.length===0 &&90+'%'}}>
           <div className={styles.profileimg}>
             <div className={styles.leftimg}>    
-              <input type="file" accept="image/*" hidden  name="profilepicture" id="profilepicture" onChange={handleProfilePicture}/>
-              <label htmlFor="profilepicture">
-                <svg xmlns="http://www.w3.org/2000/svg"  className="bi bi-camera" viewBox="0 0 16 16">
-                  <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4z"/>
-                  <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>
-                </svg>
-              </label>
-              {imgPreview? 
-              <div className={styles.profileimgcontainer} style={{backgroundImage:`url(${imgPreview})`}}>
+              <div className={styles.imgcontainer}>
+                <input type="file" accept="image/*" hidden  name="profilepicture" id="profilepicture" onChange={handleProfilePicture}/>
+                <label htmlFor="profilepicture">
+                  <svg xmlns="http://www.w3.org/2000/svg"  className="bi bi-camera" viewBox="0 0 16 16">
+                    <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4z"/>
+                    <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>
+                  </svg>
+                </label>
+                {imgPreview? 
+                <div className={styles.profileimgcontainer} style={{backgroundImage:`url(${imgPreview})`}}>
+                </div>
+                : <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-person-circle" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                </svg>}
               </div>
-               : <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-person-circle" viewBox="0 0 16 16">
-              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-              <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-              </svg>}
               <div className={styles.imgtext}>
                 <p className={styles.name}>{user?.username}</p>
                 <p className={styles.email}><svg xmlns="http://www.w3.org/2000/svg"  class="bi bi-envelope-at" viewBox="0 0 16 16">
